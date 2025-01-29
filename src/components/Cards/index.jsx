85bf02837ea4f359fabe06498d7f09e9deb35132
KeroBeros68 <kevinbertrand68.pro@gmail.com>
2025-01-29 21:46:46 +0100

    Corection css, finalisation gallery et cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,7 +1,18 @@
+import PropTypes from 'prop-types'
 import '../../styles/card.scss'
 
-function Card() {
-  return <article className="card">coucou</article>
+function Card({ title, cover }) {
+  console.log(title)
+  return (
+    <article className="card">
+      <img className="card__img" src={cover}></img>
+      <h2 className="card__title">{title}</h2>
+    </article>
+  )
 }
 
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+}
 export default Card
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,15 +1,20 @@
 import Card from '../Cards'
 import '../../styles/gallery.scss'
+import { useEffect, useState } from 'react'
 
 function Gallery() {
+  const [lodges, setLodges] = useState([])
+
+  useEffect(() => {
+    fetch('/data.json') // Chemin depuis `public/`
+      .then((response) => response.json()) // Convertit en JSON
+      .then((json) => setLodges(json)) // Stocke les données dans le state
+  }, [])
   return (
     <div className="gallery">
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
-      <Card></Card>
+      {lodges.map((lodge) => (
+        <Card key={`${lodge.id}`} title={lodge.title} cover={lodge.cover} />
+      ))}
     </div>
   )
 }
